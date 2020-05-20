f673b2a763542d38aabc2086879e9d7b3e7ac275
Tomas <45800655+lithualien@users.noreply.github.com>
2020-05-20 17:14:34 +0300

    Initial Project Add.

---
+++ src/test/java/com/github/lithualien/petclinicremastered/PetClinicRemasteredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.petclinicremastered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicRemasteredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
