cc25b10a3438f53a11848e71a1eb4857759268e2
Tomas <45800655+lithualien@users.noreply.github.com>
2020-05-20 17:35:26 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/com/github/lithualien/petclinicremastered/model/Owner.java
@@ -0,0 +1,4 @@
+package com.github.lithualien.petclinicremastered.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/github/lithualien/petclinicremastered/model/PetType.java
@@ -0,0 +1,20 @@
+package com.github.lithualien.petclinicremastered.model;
+
+public class PetType {
+    private String name;
+
+    public PetType() {
+    }
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
