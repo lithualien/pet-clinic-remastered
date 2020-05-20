849df82e6a74a0ad723c9f0e618fd8096fa3dce3
Tomas <45800655+lithualien@users.noreply.github.com>
2020-05-20 18:14:11 +0300

    refactoring to multi-module maven project. Closes #1.

---
+++ pet-clinic-web/src/main/java/com/github/lithualien/petclinicremastered/PetClinicRemasteredApplication.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.petclinicremastered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicRemasteredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicRemasteredApplication.class, args);
+    }
+
+}
