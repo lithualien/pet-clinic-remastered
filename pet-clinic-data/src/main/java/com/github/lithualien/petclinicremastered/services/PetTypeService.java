a15e3fa0b6e960860f347c94818c47dd79c07f47
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-20 23:25:21 +0300

    added PetTypeService and PetTypeServiceMap, closes #29

---
+++ pet-clinic-data/src/main/java/com/github/lithualien/petclinicremastered/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.github.lithualien.petclinicremastered.services;
+
+import com.github.lithualien.petclinicremastered.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
