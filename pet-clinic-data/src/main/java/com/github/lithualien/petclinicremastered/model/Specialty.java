8e25df09dcaad626abca1da8f0d64d9527c88f3c
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-20 23:06:09 +0300

    Added Specialty, updated Vet association with Specialty, closes #

---
+++ pet-clinic-data/src/main/java/com/github/lithualien/petclinicremastered/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.github.lithualien.petclinicremastered.model;
+
+public class Specialty {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
