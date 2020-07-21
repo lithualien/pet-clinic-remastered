b607851a2493dbbd55888fec82e2f9fa4a866a95
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-21 10:36:07 +0300

    fixed broken links, closes #45.

--- pet-clinic-web/src/main/java/com/github/lithualien/petclinicremastered/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/github/lithualien/petclinicremastered/controllers/VetController.java
@@ -14,10 +14,9 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
-
         return "vets/index";
     }
 
