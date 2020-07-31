2b712dbca33457d6cb113e1c639d741462a7bf8b
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-31 18:06:35 +0300

    Created Owner spring data jpa, closes #40.

--- pet-clinic-data/src/main/java/com/github/lithualien/petclinicremastered/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/github/lithualien/petclinicremastered/repositories/OwnerRepository.java
@@ -4,6 +4,11 @@ import com.github.lithualien.petclinicremastered.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByLastName(String lastName);
+
 }
